f51146af1de0272e1915af3a85961ce13cdd1539
y.buzueva <y.buzueva2002@mail.ru>
2021-07-05 09:56:41 +0300

    Структура,вывод

--- Критический/Rech.cs
+++ Критический/Rech.cs
@@ -2,12 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Критический
 {
     public class Rech
     {
+        string s = "";
+        public int max, mind;
+        public List<List<Rbt>> fnlcn = new List<List<Rbt>>();
+        public List<Rbt> ret;
+        public  List<Rbt> ls = Flrd(path);
+        public void put()
+        {
+            ret = ls.FindAll(x => x.point1 == ls[Minel(ls)].point1);
+            foreach (Rbt rb in ret)
+            {
+                Mv(ls, rb);
+                fnlcn.Add(RtPrs(ls, s));
+                s = "";
+            }
+        }
+        public struct Rbt
+        {//Точки, длина
+            public int point1;
+            public int point2;
+            public int length;
+
+            //Запись пути
+            public override string ToString()
+            {
+                return point1.ToString() + " - " + point2.ToString() + " " + length.ToString();
+            }
+        }
+        public void ViVod()
+        {
+            using (StreamWriter sr = new StreamWriter("Otvet.csv"))
+            {
+                foreach (Rbt rb in fnlcn[maxind])
+                {
+                    sr.WriteLine(rb.point1 + " - " + rb.point2);
+                }
+                sr.WriteLine("Rasstoanie: {0}", max);
+            }
+        }
+        public void naxodres()
+        {
+            mind = 0;
+            max = fnlcn[0][0].length;
+            ret();
+            for (int i = 0; i < ret.Count; i++)
+            {
+                if (FnlMv(fnlcn[i]) >= max)
+                {
+                    max = FnlMv(fnlcn[i]);
+                    maxind = i;
+                }
+            }
+            ViVod();
+        }
 
     }
 }
